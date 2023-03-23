779a6c54ba7fbed40e1ca56a8a18dbb89fad13cd
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2023-03-23 16:11:50 -0400

    fetched all the events + set the events variable

--- demo-events-frontend/src/App.vue
+++ demo-events-frontend/src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Events</v-toolbar-title>
+      <v-toolbar-title>Events Demo Vue Project</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
     <v-main>
-      <template>
-        <v-data-table :headers="headers" :items="events">
-          <template v-slot:items="props">
-            <td>{{ props.item.name }}</td>
-            <td>{{ props.item.time }}</td>
-          </template>
-        </v-data-table></template
-      >
+      <v-data-table :headers="headers" :items="events">
+        <template v-slot:items="props">
+          <td>{{ props.item.name }}</td>
+          <td>{{ props.item.time }}</td>
+        </template>
+      </v-data-table>
     </v-main>
   </v-app>
 </template>
@@ -26,15 +24,12 @@ interface Event {
   name: string;
   time: Date;
 }
-
+const BACKEND_URL = "http://localhost:80";
 export default Vue.extend({
   name: "App",
   // THIS IS WHERE WE CAN DEFINE ALL OF OUR REACTIVE ELEMENTS
   data: () => ({
-    events: [
-      { name: "This Is A Test", time: new Date() },
-      { name: "Second Test", time: new Date() },
-    ],
+    events: [],
     headers: [
       {
         text: "Name",
@@ -46,9 +41,15 @@ export default Vue.extend({
       },
     ],
   }),
+
   // MOUNTED IS EQUIVALENT TO USEEFFECT IN REACT
-  mounted: function () {
-    console.log(this.events);
+  mounted: async function () {
+    const response = await fetch(`${BACKEND_URL}/events`);
+    const json = await response.json();
+    json.events.forEach((item: any) => {
+      item.time = new Date(item.time);
+    });
+    this.events = json.events;
   },
 });
 </script>

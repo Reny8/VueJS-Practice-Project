17cbb6dc972b7f31f0c9aa07e5f7ae58e3a8d8ae
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2023-03-23 10:25:32 -0400

    Added the initial files to start vue js project

---
+++ demo-events-frontend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // THIS ALLOWS  THE VARIABLE TO HAVE ONE NAME. THE DEFAULT IS TWO
+    "vue/multi-word-component-names": 0,
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+};
